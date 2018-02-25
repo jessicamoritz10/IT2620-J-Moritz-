8cc3ad7751862b084c3954b4039daba4464f1984
Jessica <s00944199@acad.tri-c.edu>
2018-02-24 19:10:36 -0700

    Lab05-LargerSmaller Finished

---
+++ Lab05-LargerSmaller/Lab05-LargerSmaller/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtIntegerA = New System.Windows.Forms.TextBox()
+        Me.txtIntegerB = New System.Windows.Forms.TextBox()
+        Me.btnCompare = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(186, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "B:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(29, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "A:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(106, 25)
+        Me.Label3.MinimumSize = New System.Drawing.Size(160, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(160, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Enter Two Integers"
+        '
+        'txtIntegerA
+        '
+        Me.txtIntegerA.Location = New System.Drawing.Point(60, 67)
+        Me.txtIntegerA.Name = "txtIntegerA"
+        Me.txtIntegerA.Size = New System.Drawing.Size(100, 20)
+        Me.txtIntegerA.TabIndex = 3
+        '
+        'txtIntegerB
+        '
+        Me.txtIntegerB.Location = New System.Drawing.Point(217, 67)
+        Me.txtIntegerB.Name = "txtIntegerB"
+        Me.txtIntegerB.Size = New System.Drawing.Size(100, 20)
+        Me.txtIntegerB.TabIndex = 4
+        '
+        'btnCompare
+        '
+        Me.btnCompare.Font = New System.Drawing.Font("Modern No. 20", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCompare.Location = New System.Drawing.Point(75, 153)
+        Me.btnCompare.Name = "btnCompare"
+        Me.btnCompare.Size = New System.Drawing.Size(75, 23)
+        Me.btnCompare.TabIndex = 5
+        Me.btnCompare.Text = "Compare"
+        Me.btnCompare.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Modern No. 20", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(200, 153)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResults.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblResults.Location = New System.Drawing.Point(29, 120)
+        Me.lblResults.MinimumSize = New System.Drawing.Size(300, 20)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(300, 20)
+        Me.lblResults.TabIndex = 7
+        Me.lblResults.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(362, 230)
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCompare)
+        Me.Controls.Add(Me.txtIntegerB)
+        Me.Controls.Add(Me.txtIntegerA)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Larger and Smaller"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Private WithEvents Label3 As Label
+    Friend WithEvents txtIntegerA As TextBox
+    Friend WithEvents txtIntegerB As TextBox
+    Friend WithEvents btnCompare As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblResults As Label
+End Class
---
+++ Lab05-LargerSmaller/Lab05-LargerSmaller/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
+
+    Private Sub btnCompare_Click(sender As Object, e As EventArgs) Handles btnCompare.Click
+        ' Declare variables.
+        Dim intA As Integer
+        Dim intB As Integer
+        lblResults.Text = String.Empty
+
+        If Integer.TryParse(txtIntegerA.Text, intA) = False Then
+            lblResults.Text = "Enter a valid integer."
+        End If
+        If Integer.TryParse(txtIntegerB.Text, intB) = False Then
+            lblResults.Text = "Enter a valid integer."
+        End If
+        'Determine which number is greater
+        If intA > intB Then
+            lblResults.Text = "Integer A is greater."
+        End If
+        If intA < intB Then
+                lblResults.Text = "Integer B is greater."
+            End If
+
+    End Sub
+End Class
