9780e484ad635e22c76dea66be1ee88586aad4bb
Jessica <s00944199@acad.tri-c.edu>
2018-02-26 15:11:37 -0700

    Lab06-SumofNumbers finished

---
+++ Lab06-SumofNumbers/Lab06-SumofNumbers/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Modern No. 20", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(26, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Enter Max Value"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Modern No. 20", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(170, 37)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(120, 35)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(316, 103)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Sum of Numbers"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Lab06-SumofNumbers/Lab06-SumofNumbers/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Declare a variable to store user input string from the input box
+        Dim strInputString As String = InputBox(
+            "Enter a positive integer value.", "Input Needed", "10")
+
+        ' Declare a variable to store the user input.
+        Dim intInputNumber As Integer
+
+        ' Initialize a variable to store sum of the numberse.
+        Dim intSum As Integer = 0
+
+        If Integer.TryParse(strInputString, intInputNumber) And
+             intInputNumber >= 0 Then
+
+            'Use For Loop ro sum of all values from 1 through the value entered by user.
+            Dim intCounter As Integer
+            For intCounter = 1 To intInputNumber
+                intSum += intCounter
+            Next
+            ' Display a message to the user indicating sum.
+            MessageBox.Show("The sum of numbers 1 through" &
+                            strInputString & " is " &
+                            intSum.ToString("d"), "Sum of Numbers")
+
+        Else
+            ' Error message for invalid input.
+            MessageBox.Show("Enter a positive integer value.", "Invalid Input")
+        End If
+
+    End Sub
+End Class
