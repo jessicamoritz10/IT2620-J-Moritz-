9e136207e4bbe92bc568a6774b4015b4cd1d3908
Jessica <s00944199@acad.tri-c.edu>
2018-03-05 16:49:00 -0700

    Retail Calc Done

---
+++ Lab06RetailCalculator/Lab06RetailCalculator/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.txtMarkup = New System.Windows.Forms.TextBox()
+        Me.txtWholesale = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(50, 226)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 0
+        Me.btnCalc.Text = "Get Retail"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(199, 226)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(116, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Wholesale Cost of Item"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(91, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Markup %"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(84, 130)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Retail Price"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPrice.Location = New System.Drawing.Point(171, 130)
+        Me.lblPrice.MinimumSize = New System.Drawing.Size(100, 20)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(100, 20)
+        Me.lblPrice.TabIndex = 5
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Location = New System.Drawing.Point(71, 174)
+        Me.lblMessage.MinimumSize = New System.Drawing.Size(200, 18)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(200, 18)
+        Me.lblMessage.TabIndex = 6
+        '
+        'txtMarkup
+        '
+        Me.txtMarkup.Location = New System.Drawing.Point(174, 73)
+        Me.txtMarkup.Name = "txtMarkup"
+        Me.txtMarkup.Size = New System.Drawing.Size(100, 20)
+        Me.txtMarkup.TabIndex = 7
+        '
+        'txtWholesale
+        '
+        Me.txtWholesale.Location = New System.Drawing.Point(174, 21)
+        Me.txtWholesale.Name = "txtWholesale"
+        Me.txtWholesale.Size = New System.Drawing.Size(100, 20)
+        Me.txtWholesale.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(329, 261)
+        Me.Controls.Add(Me.txtWholesale)
+        Me.Controls.Add(Me.txtMarkup)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "Form1"
+        Me.Text = "Retail Price Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents txtMarkup As TextBox
+    Friend WithEvents txtWholesale As TextBox
+End Class
---
+++ Lab06RetailCalculator/Lab06RetailCalculator/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+
+    Function CalculateRetail(ByVal dblWholesaleCost As Double,
+             ByVal dblMarkupPercentage As Double) As Double
+
+        Return dblWholesaleCost + dblWholesaleCost * (dblMarkupPercentage * 0.01)
+
+    End Function
+
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        'Local variables
+        Dim dblWholesaleCost As Double
+        Dim dblMarkupPercentage As Double
+        Dim dblRetailPrice As Double
+
+        lblMessage.Text = String.Empty
+
+        If Double.TryParse(txtWholesale.Text, dblWholesaleCost) Then
+
+            If Double.TryParse(txtMarkup.Text, dblMarkupPercentage) Then
+
+
+                dblRetailPrice = CalculateRetail(dblWholesaleCost, dblMarkupPercentage)
+                lblPrice.Text = dblRetailPrice.ToString("c")
+
+            Else
+                lblMessage.Text = "Please enter a positive markup percentage"
+            End If
+
+        Else
+            lblMessage.Text = "Please enter positive wholesale cost"
+        End If
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
