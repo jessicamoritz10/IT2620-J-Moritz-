4903c8b76410f54526a3e43c5f77ff95a722a765
Jessica <s00944199@acad.tri-c.edu>
2018-03-05 21:16:23 -0700

    Password Verifier done

---
+++ Lab06PasswordVerifier/Lab06PasswordVerifier/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(144, 104)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 0
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(35, 226)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 1
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(169, 226)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(57, 107)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Password:"
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Location = New System.Drawing.Point(44, 169)
+        Me.lblMessage.MinimumSize = New System.Drawing.Size(200, 18)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(200, 18)
+        Me.lblMessage.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(57, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(187, 64)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password Criteria:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "1. At least 6 characters long" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "2. At least one numerical digi" &
+    "t" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "3. At Least one alphabetic character"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Name = "Form1"
+        Me.Text = "Password Verifier"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Lab06PasswordVerifier/Lab06PasswordVerifier/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Function IsValid(strPassword As String) As Boolean
+        Dim pwPart As String
+        Dim I As Integer
+        Dim L As Integer
+        Dim lengthOK, ContainsNumber, ContainsAlpha As Boolean
+        L = strPassword.Length
+
+        If L < 6 Then
+            lengthOK = False
+            Return False
+        End If
+
+        I = 0
+        For I = 1 To L
+            pwPart = Mid(strPassword, I, 1)
+            If IsNumeric(pwPart) = True Then
+                ContainsNumber = True
+            End If
+        Next
+        I = 0
+        For I = 1 To L
+            pwPart = Mid(strPassword, I, 1)
+            If IsNumeric(pwPart) = False Then
+                ContainsAlpha = True
+            End If
+        Next
+        If ContainsAlpha = True And ContainsNumber = True Then
+            Return True
+        End If
+
+    End Function
+
+
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim password As String
+        password = txtPassword.Text
+
+        If IsValid(passWord) = True Then
+            lblMessage.Text = "That password is valid"
+        Else
+            lblMessage.Text = "Password is invalid"
+        End If
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
