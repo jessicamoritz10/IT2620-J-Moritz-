ab5c4fd72940bea2f4edf99c740f59982af4ae05
Jessica <s00944199@acad.tri-c.edu>
2018-04-11 15:48:31 -0600

    Tic-Tac-Toe Finished

---
+++ Lab11TicTacToe/Lab11TicTacToe/Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblWin = New System.Windows.Forms.Label()
+        Me.btnNewGame = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.lbl5 = New System.Windows.Forms.Label()
+        Me.lbl7 = New System.Windows.Forms.Label()
+        Me.lbl8 = New System.Windows.Forms.Label()
+        Me.lbl9 = New System.Windows.Forms.Label()
+        Me.lbl6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl1.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl1.Location = New System.Drawing.Point(41, 9)
+        Me.lbl1.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl1.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl1.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl1.Size = New System.Drawing.Size(70, 75)
+        Me.lbl1.TabIndex = 0
+        Me.lbl1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(162, 24)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 3
+        '
+        'lblWin
+        '
+        Me.lblWin.AutoSize = True
+        Me.lblWin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblWin.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWin.Location = New System.Drawing.Point(72, 290)
+        Me.lblWin.MinimumSize = New System.Drawing.Size(200, 20)
+        Me.lblWin.Name = "lblWin"
+        Me.lblWin.Size = New System.Drawing.Size(200, 20)
+        Me.lblWin.TabIndex = 9
+        Me.lblWin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnNewGame
+        '
+        Me.btnNewGame.Location = New System.Drawing.Point(87, 337)
+        Me.btnNewGame.Name = "btnNewGame"
+        Me.btnNewGame.Size = New System.Drawing.Size(75, 23)
+        Me.btnNewGame.TabIndex = 10
+        Me.btnNewGame.Text = "New Game"
+        Me.btnNewGame.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(188, 337)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl2.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl2.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl2.Location = New System.Drawing.Point(142, 9)
+        Me.lbl2.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl2.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl2.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl2.Size = New System.Drawing.Size(70, 75)
+        Me.lbl2.TabIndex = 12
+        Me.lbl2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl3.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl3.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl3.Location = New System.Drawing.Point(244, 9)
+        Me.lbl3.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl3.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl3.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl3.Size = New System.Drawing.Size(70, 75)
+        Me.lbl3.TabIndex = 13
+        Me.lbl3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl4.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl4.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl4.Location = New System.Drawing.Point(41, 101)
+        Me.lbl4.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl4.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl4.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl4.Size = New System.Drawing.Size(70, 75)
+        Me.lbl4.TabIndex = 14
+        Me.lbl4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl5
+        '
+        Me.lbl5.AutoSize = True
+        Me.lbl5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl5.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl5.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl5.Location = New System.Drawing.Point(142, 101)
+        Me.lbl5.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl5.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl5.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl5.Name = "lbl5"
+        Me.lbl5.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl5.Size = New System.Drawing.Size(70, 75)
+        Me.lbl5.TabIndex = 15
+        Me.lbl5.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl7
+        '
+        Me.lbl7.AutoSize = True
+        Me.lbl7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl7.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl7.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl7.Location = New System.Drawing.Point(41, 195)
+        Me.lbl7.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl7.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl7.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl7.Name = "lbl7"
+        Me.lbl7.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl7.Size = New System.Drawing.Size(70, 75)
+        Me.lbl7.TabIndex = 16
+        Me.lbl7.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl8
+        '
+        Me.lbl8.AutoSize = True
+        Me.lbl8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl8.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl8.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl8.Location = New System.Drawing.Point(142, 195)
+        Me.lbl8.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl8.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl8.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl8.Name = "lbl8"
+        Me.lbl8.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl8.Size = New System.Drawing.Size(70, 75)
+        Me.lbl8.TabIndex = 17
+        Me.lbl8.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl9
+        '
+        Me.lbl9.AutoSize = True
+        Me.lbl9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl9.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl9.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl9.Location = New System.Drawing.Point(244, 195)
+        Me.lbl9.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl9.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl9.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl9.Name = "lbl9"
+        Me.lbl9.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl9.Size = New System.Drawing.Size(70, 75)
+        Me.lbl9.TabIndex = 18
+        Me.lbl9.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl6
+        '
+        Me.lbl6.AutoSize = True
+        Me.lbl6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl6.Font = New System.Drawing.Font("Stencil", 50.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl6.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lbl6.Location = New System.Drawing.Point(244, 101)
+        Me.lbl6.Margin = New System.Windows.Forms.Padding(0)
+        Me.lbl6.MaximumSize = New System.Drawing.Size(75, 75)
+        Me.lbl6.MinimumSize = New System.Drawing.Size(70, 70)
+        Me.lbl6.Name = "lbl6"
+        Me.lbl6.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbl6.Size = New System.Drawing.Size(70, 75)
+        Me.lbl6.TabIndex = 19
+        Me.lbl6.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(350, 374)
+        Me.Controls.Add(Me.lbl6)
+        Me.Controls.Add(Me.lbl9)
+        Me.Controls.Add(Me.lbl8)
+        Me.Controls.Add(Me.lbl7)
+        Me.Controls.Add(Me.lbl5)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnNewGame)
+        Me.Controls.Add(Me.lblWin)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lbl1)
+        Me.Name = "Form1"
+        Me.Text = "Tic-Tac-Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblWin As Label
+    Friend WithEvents btnNewGame As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents lbl5 As Label
+    Friend WithEvents lbl7 As Label
+    Friend WithEvents lbl8 As Label
+    Friend WithEvents lbl9 As Label
+    Friend WithEvents lbl6 As Label
+End Class
---
+++ Lab11TicTacToe/Lab11TicTacToe/Form1.vb
@@ -0,0 +1,122 @@
+﻿Public Class Form1
+    Dim game(2, 2) As Integer
+    Const intMAX_ROW As Integer = 2
+    Const intMAX_COL As Integer = 2
+    Dim rand As New Random
+    Dim intRow As Integer
+    Dim intCol As Integer
+    'CheckForWin function to check for a valid winner.
+    Public Function CheckForWin(value As Integer) As Boolean
+        Dim hasWin As Boolean
+
+        'Check each row
+
+        For intRow As Integer = 0 To intMAX_ROW
+            hasWin = True
+            For intCol As Integer = 0 To intMAX_COL
+                If game(intRow, intCol) <> value Then
+                    hasWin = True
+                End If
+            Next
+        Next
+
+        'Check each column
+        For intCol As Integer = 0 To intMAX_COL
+            hasWin = True
+            For intRow As Integer = 0 To intMAX_ROW
+                If game(intCol, intRow) <> value Then
+                    hasWin = False
+                End If
+            Next
+        Next
+
+        'Check diagonal top right to bottom left
+        For intRow As Integer = 0 To intMAX_ROW
+            hasWin = True
+            If game(intRow, intRow) <> value Then
+                hasWin = False
+            End If
+        Next
+
+        'Check diagonal top left to bottom right
+        For intRow As Integer = 0 To intMAX_ROW
+            hasWin = True
+            If game(intRow, 2 - intRow) <> value Then
+                hasWin = False
+            End If
+        Next
+        If hasWin = True Then Return True
+        Return False
+    End Function
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form
+        Me.Close()
+    End Sub
+
+    Private Sub btnNewGame_Click(sender As Object, e As EventArgs) Handles btnNewGame.Click
+        'Randomly fill array with a value of 0 or 1
+        For intRow As Integer = 0 To 2
+            For intCol As Integer = 0 To 2
+                Dim n As Integer = rand.Next(2)
+                game(intRow, intCol) = n
+            Next
+        Next
+        'Determine if an X or O will be the text by the random value placed in the subscript.
+        If game(0, 0) = 1 Then
+            lbl1.Text = "X"
+        Else
+            lbl1.Text = "O"
+        End If
+        If game(0, 1) = 1 Then
+            lbl2.Text = "X"
+        Else
+            lbl2.Text = "O"
+        End If
+        If game(0, 2) = 1 Then
+            lbl3.Text = "X"
+        Else
+            lbl3.Text = "O"
+        End If
+        If game(1, 0) = 1 Then
+            lbl4.Text = "X"
+        Else
+            lbl4.Text = "O"
+        End If
+        If game(1, 1) = 1 Then
+            lbl5.Text = "X"
+        Else
+            lbl5.Text = "O"
+        End If
+        If game(1, 2) = 1 Then
+            lbl6.Text = "X"
+        Else
+            lbl6.Text = "O"
+        End If
+        If game(2, 0) = 1 Then
+            lbl7.Text = "X"
+        Else
+            lbl7.Text = "O"
+        End If
+        If game(2, 1) = 1 Then
+            lbl8.Text = "X"
+        Else
+            lbl8.Text = "O"
+        End If
+        If game(2, 2) = 1 Then
+            lbl9.Text = "X"
+        Else
+            lbl9.Text = "O"
+        End If
+        'Call CheckForWin function to determine if game is a tie or a player won.
+        Dim winnerzero As Boolean = CheckForWin(0) 'check for an O's winner by value 0
+        Dim winnerone As Boolean = CheckForWin(1) 'check for an X's winner by value 3
+        If winnerzero And winnerone Then
+            lblWin.Text = "Tie game!"
+        ElseIf winnerzero And Not winnerone Then
+            lblWin.Text = "O WINS!"
+        Else lblWin.Text = "X WINS!"
+
+        End If
+    End Sub
+End Class
